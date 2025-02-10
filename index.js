b0629a62a19cb4b0780a1f954c7093811f0421ad
Luciano Seguel <lucianitoseguel@gmail.com>
2025-02-10 00:05:29 -0300

    1.3

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase
 import { getStorage } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-storage.js';
 import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-analytics.js';
 import { getAuth } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js';
-
+import {dotenv} from "dotenv"
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-let key = process.env.APIKEY
+const key = process.env.APIKEY
 
 const firebaseConfig = {
     apiKey: key,
